6ab113dd0b4e35e4143fd25f98dfd18424a74a9c
Sarwar-Putul <sarwarputul@gmail.com>
2021-01-30 19:20:21 +0600

    Jubair

--- 03 Cruise Queen/script.js
+++ 03 Cruise Queen/script.js
@@ -47,6 +47,8 @@
             const firstCount = parseInt(firstInput.value);
             const ecoInput = document.getElementById('eco-count');
             const ecoCount = parseInt(ecoInput.value);
+            const firstCount = firstNewCount;
+            const ecoCount = ecoNewCount;
             const totalPrice = firstNewCount * 150 + ecoNewCount * 100;
             document.getElementById('total-price').innerText = '$' + totalPrice;
    
@@ -65,3 +67,83 @@
             const confirmTotal = totalPrice + tax;
             document.getElementById('confirm-total').innerText = '$' + confirmTotal;
          }
+
+
+
+
+
+
+         const bookingbtn = document.getElementById('booked');
+         bookingbtn.addEventListener('click',function(){
+             const bookingArea = document.getElementById('booking-area');
+             bookingArea.style.display = "none";
+             const confirmationArea = document.getElementById('confirmation-area');
+             confirmationArea.style.display = "block";
+ 
+         })
+     //      function handleCruiseChange(cruise, isIncrease) {
+     //      const cruiseInput = document.getElementById(cruise +'-count');
+     //      const cruiseCount = parseInt(cruiseInput.value);
+     //         let cruiseNewCount = cruiseCount;
+     //         if(isIncrease == true) {
+     //             cruiseNewCount = cruisetCount + 1;
+     //         }
+     //         if(isIncrease == false && cruiseCount > 0) {
+     //             cruiseNewCount = cruiseCount -1;
+     //         }
+     //         cruiseInput.value = cruiseNewCount;
+     //         let cruiseTotal = 0;
+     //         if(cruise == 'firstClass'){
+     //             cruiseTotal = cruiseNewCount * 150;
+     //         }
+     //         if (cruise == 'economy'){
+     //             cruiseTotal = cruiseNewCount * 100;
+     //         }
+     //         document.getElementById('cruise').innerText = cruiseTotal;
+     //         calculateTotal()
+     //   }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+         // document.getElementById('first-increase').addEventListener('click', function(){
+         //     const firstInput = document.getElementById('first-count');
+         //     const firstCount = parseInt(firstInput.value);
+         //     const firstNewCount = firstCount + 1;
+         //     firstInput.value = firstNewCount;
+         //     const firstClassTotal = firstNewCount * 150;
+         //     document.getElementById('total-price').innerText = '$' + firstClassTotal;
+         // });
+ 
+         // document.getElementById('first-decrease').addEventListener('click', function(){
+         //     const firstInput = document.getElementById('first-count');
+         //     const firstCount = parseInt(firstInput.value);
+         //     const firstNewCount = firstCount - 1;
+         //     firstInput.value = firstNewCount;
+         //     const firstClassTotal = firstNewCount * 150;
+         //  document.getElementById('total-price').innerText = '$' + firstClassTotal;
+         // });
+ 
+         // document.getElementById('eco-increase').addEventListener('click', function(){
+         //     const ecoInput = document.getElementById('eco-count');
+         //     const ecoCount = parseInt(ecoInput.value);
+         //     const ecoNewCount = ecoCount + 1;
+         //     ecoInput.value = ecoNewCount;
+         //     const economyTotal = ecoNewCount * 100;
+         // });
+ 
+         // document.getElementById('eco-decrease').addEventListener('click', function(){
+         //     const ecoInput = document.getElementById('eco-count');
+         //     const ecoCount = parseInt(ecoInput.value);
+         //     const ecoNewCount = ecoCount - 1;
+         //     ecoInput.value = ecoNewCount;
+         //     const economyTotal = ecoNewCount * 100;
+         // });
